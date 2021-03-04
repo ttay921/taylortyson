4914e05e3c7582ec1a64a03562a9bc59cd870606
Taylor Tyson1 <ttay921@live.unc.edu>
2021-03-04 10:58:28 -0500

    New things

---
+++ precache-manifest.73d49977d49c582bb9da2b286f8755e3.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3734a4c3c66308b1cc2ebf2b6fc5cf07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c91a87b46084d4cb991a",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "cc96fd93b3c2ba4ba555",
+    "url": "/static/css/main.b26160c3.chunk.css"
+  },
+  {
+    "revision": "c91a87b46084d4cb991a",
+    "url": "/static/js/2.cc0cf072.chunk.js"
+  },
+  {
+    "revision": "1f10136a3131f37e5155ac8f5c05e03f",
+    "url": "/static/js/2.cc0cf072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc96fd93b3c2ba4ba555",
+    "url": "/static/js/main.ab995d28.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "68a897ff8debad6f0a2abf6857a95a1b",
+    "url": "/static/media/MAK.68a897ff.woff"
+  },
+  {
+    "revision": "8e809e3d13bd25a184fe25c231ea6f6f",
+    "url": "/static/media/MAK.8e809e3d.otf"
+  },
+  {
+    "revision": "e0b9ae5513323011e1af7d36981e0465",
+    "url": "/static/media/MAK.e0b9ae55.eot"
+  },
+  {
+    "revision": "492beb66a933f8ea2d65d33eae6d2374",
+    "url": "/static/media/PhotoStory.492beb66.gif"
+  },
+  {
+    "revision": "b3cecf71626ea3ea8616454f13deb341",
+    "url": "/static/media/StyleTile.b3cecf71.png"
+  },
+  {
+    "revision": "e0e0398fd68c24b3155aee594f788129",
+    "url": "/static/media/StyleTile.e0e0398f.jpg"
+  },
+  {
+    "revision": "a24534b471d337f00263f5bbfada56db",
+    "url": "/static/media/am.a24534b4.png"
+  },
+  {
+    "revision": "cab087db1e0483f3aeb7705061dd8651",
+    "url": "/static/media/cap.cab087db.png"
+  },
+  {
+    "revision": "570f89b569407f79516114478ef8b60d",
+    "url": "/static/media/deltapsi.570f89b5.png"
+  },
+  {
+    "revision": "4546936919f1591ba7d4e73cc0edd4d0",
+    "url": "/static/media/extras-01.45469369.png"
+  },
+  {
+    "revision": "87ec902fd125f2e5cca02f5e8d67e049",
+    "url": "/static/media/extras-02.87ec902f.png"
+  },
+  {
+    "revision": "dedac8b608f4c1e612af47cf9aca647e",
+    "url": "/static/media/extras-04.dedac8b6.png"
+  },
+  {
+    "revision": "6c0cf48cdc5f7baaa8400c1090869fc1",
+    "url": "/static/media/fakesnap.6c0cf48c.png"
+  },
+  {
+    "revision": "d67ccaddd74b9e41e9f70acec4fefa1e",
+    "url": "/static/media/fallfest11.d67ccadd.jpg"
+  },
+  {
+    "revision": "bb199db8f14480a554813e5f4c99224c",
+    "url": "/static/media/flyer.bb199db8.jpg"
+  },
+  {
+    "revision": "28486e851536d7bfc857020d76881e47",
+    "url": "/static/media/flyersummer.28486e85.jpg"
+  },
+  {
+    "revision": "0fbc43d754c1ca63ea30b7ffd462c6f9",
+    "url": "/static/media/fpg.0fbc43d7.png"
+  },
+  {
+    "revision": "d1ed909c28276c9a0eb58c08806f4b4f",
+    "url": "/static/media/hospitaldata.d1ed909c.png"
+  },
+  {
+    "revision": "486d753c148454dab43ec93741048f41",
+    "url": "/static/media/hypebbg.486d753c.png"
+  },
+  {
+    "revision": "1bfaff8b9798742558b1bc81529c67ab",
+    "url": "/static/media/ig-Story.1bfaff8b.png"
+  },
+  {
+    "revision": "2a59725c8801ab7c89c2e5bc3b510ee1",
+    "url": "/static/media/internal.2a59725c.png"
+  },
+  {
+    "revision": "d377ac15a1c5815566145a5b1912acad",
+    "url": "/static/media/logo-animation.d377ac15.png"
+  },
+  {
+    "revision": "90b46d8d0cd9d3e3ccad7e4aeaf55084",
+    "url": "/static/media/mainchar.90b46d8d.png"
+  },
+  {
+    "revision": "47feec25059defa83ca1e3c7ec01cac9",
+    "url": "/static/media/map.47feec25.png"
+  },
+  {
+    "revision": "8e3097b656bbd1089d934f286aceb778",
+    "url": "/static/media/moonlight.8e3097b6.png"
+  },
+  {
+    "revision": "5669e2c0385903b94d842e95b4912ee1",
+    "url": "/static/media/nations.5669e2c0.png"
+  },
+  {
+    "revision": "88aefdb89d5c6819ec5b54b621afde23",
+    "url": "/static/media/newheadshot.88aefdb8.jpg"
+  },
+  {
+    "revision": "7647dff00c35eebed19a7f2483818376",
+    "url": "/static/media/newsticks.7647dff0.jpg"
+  },
+  {
+    "revision": "c5628b97072d8af08e6c9bb8d6f0685e",
+    "url": "/static/media/option.c5628b97.jpg"
+  },
+  {
+    "revision": "fb37fceaaf5cec6f537a1fccc813ee68",
+    "url": "/static/media/pamphlet.fb37fcea.png"
+  },
+  {
+    "revision": "81c6418809cfa7842ddc8d70e01ff437",
+    "url": "/static/media/panopli.81c64188.png"
+  },
+  {
+    "revision": "ec97bfdd801bfafebe776151ddd264d0",
+    "url": "/static/media/pdf-1.ec97bfdd.jpg"
+  },
+  {
+    "revision": "17cd041afdf8a5f1b6c61a1fc8241cbb",
+    "url": "/static/media/pdf-2.17cd041a.jpg"
+  },
+  {
+    "revision": "4f5bee56a6a3bfe85f317e4fc75434c2",
+    "url": "/static/media/pdf-3.4f5bee56.jpg"
+  },
+  {
+    "revision": "c4cbc121c203393c48148fc3523952b3",
+    "url": "/static/media/pdf-4.c4cbc121.jpg"
+  },
+  {
+    "revision": "8d9c4858bf6622bfb8a6c939aaad563e",
+    "url": "/static/media/pillow.8d9c4858.png"
+  },
+  {
+    "revision": "c93fc2ddf99ed07a0eaf383df6200c43",
+    "url": "/static/media/silhouette.c93fc2dd.png"
+  },
+  {
+    "revision": "2e304a7938097e69e671b4a36ef8d61c",
+    "url": "/static/media/ss1.2e304a79.png"
+  },
+  {
+    "revision": "f30b2319bfc4b053c8360becdf0d9564",
+    "url": "/static/media/ss2.f30b2319.png"
+  },
+  {
+    "revision": "08262976562e0a95fa5d2dc77d0b1fd5",
+    "url": "/static/media/ss3.08262976.png"
+  },
+  {
+    "revision": "937ca95089a291b419099f3bde1045d9",
+    "url": "/static/media/t-shirt-white.937ca950.png"
+  },
+  {
+    "revision": "246772a5bf71b72b85404e27ad5eb3b3",
+    "url": "/static/media/uncglobalcard.246772a5.png"
+  },
+  {
+    "revision": "de4c75c420fd6d70fd263098adbbbc70",
+    "url": "/static/media/uncsom.de4c75c4.png"
+  },
+  {
+    "revision": "3e1764fd236932fc2061f919986e1be5",
+    "url": "/static/media/wildfire.3e1764fd.png"
+  },
+  {
+    "revision": "329da7cc652b5cf990a5fd1004156379",
+    "url": "/static/media/wxyc.329da7cc.png"
+  }
+]);
